86d59e5a2a4d0ca72eca3f737ce9ed8bff2e34b1
NAGARJUNA D <42113105+DorthalaNagarjuna@users.noreply.github.com>
2019-11-03 00:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Diabetes level is{}'.format(output))
+    return render_template('index.html', nag=int(f'{output}'))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
