8798a2123eb149511e01059a6291bdff36ea1394
NAGARJUNA D <42113105+DorthalaNagarjuna@users.noreply.github.com>
2019-11-03 00:31:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', nag=int(f'{outpur}'))
+    return render_template('index.html', nag=int(f'{output}'))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
